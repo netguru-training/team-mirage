caa8a13419a8f7c3e724b207af78c47bb112fd03
barcadius <arkadiusz.gil95@gmail.com>
2015-12-12 13:57:18 +0100

    Repair full_name decorator method

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,8 +2,6 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def full_name
-    if first_name and last_name
-      first_name + ' ' + last_name
-    end
+    [first_name, last_name].join(' ')
   end
 end
