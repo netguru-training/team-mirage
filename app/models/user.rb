3271c881f892fc56caba32555206fc30cfa6e65a
Angelika Tyborska <atyborska93@gmail.com>
2015-12-12 13:56:30 +0100

    Refactor User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
   rolify
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   has_many :payments
   has_many :projects, foreign_key: :owner_id
   has_many :paid_projects, through: :payments, source: :project
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
