0ff80ff4f742217b36c559f5c4adcd59dea0a955
Angelika Tyborska <atyborska93@gmail.com>
2015-12-12 13:23:22 +0100

    Fix infinite recursion in UserDecorator#full_name

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,8 +2,8 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def full_name
-    if full_name and last_name
-      full_name + ' ' + last_name
+    if first_name and last_name
+      first_name + ' ' + last_name
     end
   end
 end
