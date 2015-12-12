e6e983386a7d06a3f454b9b7bb502a72e9bb712a
barcadius <arkadiusz.gil95@gmail.com>
2015-12-12 11:26:20 +0100

    Add base controller and projects controller for admin

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,5 @@
+module Admin
+  class ApplicationController < ApplicationController
+
+  end
+end
---
+++ app/controllers/admin/projects_controller.rb
@@ -0,0 +1,7 @@
+module Admin
+
+  class ProjectsController < Admin::ApplicationController
+
+  end
+
+end
