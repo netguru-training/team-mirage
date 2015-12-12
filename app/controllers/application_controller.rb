6ab2a340c2e4bf453c61f4e8c8ea6d395d9718ee
railsbeginner <bartosz.dyja@gmail.com>
2015-12-12 11:55:23 +0100

    Clean up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-<<<<<<< HEAD
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -12,10 +11,8 @@ class ApplicationController < ActionController::Base
     other_details = [:occupation, :country]
     devise_parameter_sanitizer.for(:sign_up) << names
     devise_parameter_sanitizer.for(:account_update) << names + other_details
-=======
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
->>>>>>> Configure application controller for decent exposure gem
   end
 end
