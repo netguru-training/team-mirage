b17772ebd9ef5b61568c1d73043662a251f04fae
Angelika Tyborska <atyborska93@gmail.com>
2015-12-12 20:03:58 +0100

    Remove integer validation from Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,9 +9,9 @@ class Project < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
   validates :goal, presence: true,
-                   numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+                   numericality: { greater_than_or_equal_to: 0 }
   validates :current_funds, presence: true,
-                            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+                            numericality: { greater_than_or_equal_to: 0 }
   validates :setup_date, presence: true
   validates :finish_date, presence: true
 
