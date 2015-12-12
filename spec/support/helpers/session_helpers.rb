20203544ecbc5dd2fda5637bc16be1b1e993b905
railsbeginner <bartosz.dyja@gmail.com>
2015-12-12 14:05:15 +0100

    Refactor sign in helper in specs

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -2,6 +2,8 @@ module Features
   module SessionHelpers
     def sign_up_with(email, password, confirmation)
       visit new_user_registration_path
+      fill_in 'First name', with: 'ABC'
+      fill_in 'Last name', with: 'DEF'
       fill_in 'Email', with: email
       fill_in 'Password', with: password
       fill_in 'Password confirmation', :with => confirmation
