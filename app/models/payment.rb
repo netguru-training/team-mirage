e7e6876adfe3f53f1923bd7497a12e82ba4494b8
Angelika Tyborska <atyborska93@gmail.com>
2015-12-12 17:07:54 +0100

    Refactor Payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,7 +2,7 @@ class Payment < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  validates :value, presence: true, numericality: { greater_than: 0, less_than: 2147483647}
+  validates :value, presence: true, numericality: { greater_than: 0, less_than: 2_147_483_647 }
   validates :user, presence: true
   validates :project, presence: true
   validate :project_must_be_active
