4354ba410175f94dc912c93e14d20af933b842c1
Angelika Tyborska <atyborska93@gmail.com>
2015-12-12 21:34:15 +0100

    Neaten whitespaces in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   expose(:user, attributes: :user_params)
+
   def show
   end
 
   def update
-    if current_user==user && user.save 
+    if current_user == user && user.save
       redirect_to user_path(user), notice: 'Your profile has been updated'
     else
       render :edit
@@ -16,6 +17,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name)
   end
-
-
 end
