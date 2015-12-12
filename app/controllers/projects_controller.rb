fc987389ebf50d081f75c52faffb97658922d16c
railsbeginner <bartosz.dyja@gmail.com>
2015-12-12 11:30:56 +0100

    Configure project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,25 @@
 class ProjectsController < ApplicationController
-  def new
-    @project=Project.new
+  expose(:projects)
+  expose(:project, attributes: :projects_params)
+
+  def create
+    #project.owner = current_user
+    if project.save
+      redirect_to project_path(project), notice: 'Project has been created'
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def index
+  end
+
+  private
+
+  def projects_params
+    params.require(:project).permit(:name, :description, :goal, :setup_date, :finish_date)
   end
 end
