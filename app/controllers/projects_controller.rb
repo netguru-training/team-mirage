19007972db3800275e35ed2dbe15213089202718
railsbeginner <bartosz.dyja@gmail.com>
2015-12-12 10:58:40 +0100

    Add projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,10 @@
+class ProjectsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :projects
+
   root to: 'visitors#index'
   devise_for :users, controllers: { registrations: 'users/registrations' }
 end
