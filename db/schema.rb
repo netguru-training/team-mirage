1521d9ccd39149a583e56e10ff271f96e64f337f
Angelika Tyborska <atyborska93@gmail.com>
2015-12-12 11:40:15 +0100

    Add more details to User
    Replace name with first and last names, add occupation and country.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 20151212103325) do
+=======
+ActiveRecord::Schema.define(version: 20151212102734) do
+>>>>>>> Stashed changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +54,10 @@ ActiveRecord::Schema.define(version: 20151212103325) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "occupation"
+    t.string   "country"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
