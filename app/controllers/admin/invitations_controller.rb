a03608b34a86f0d301c537a23c8f77dcf0ecef98
barcadius <arkadiusz.gil95@gmail.com>
2015-12-13 15:07:29 +0100

    Refactor new invitation action

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -3,16 +3,22 @@ module Admin
     expose(:user)
 
     def create
-      user = User.invite!(user_params) do |u|
-        u.skip_invitation = true
-      end
-      if user.save
-        User.invite!(email: user.email)
-        flash[:notice] = "Invitation sent successfully"
-        redirect_to admin_users_path
-      else
-        flash[:alert] = "There were problems with sending an invitation"
+      if User.find_by(email: params[:user][:email])
+        flash[:alert] = "User with given email is registered or has been "+
+                        "already invited!"
         render :new
+      else
+        user = User.invite!(user_params) do |u|
+          u.skip_invitation = true
+        end
+        if user.save
+          User.invite!(email: user.email)
+          flash[:notice] = "Invitation sent successfully"
+          redirect_to admin_users_path
+        else
+          flash[:alert] = "There were problems with sending an invitation"
+          render :new
+        end
       end
     end
 
