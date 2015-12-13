44d2b40957aaa1ab14c1dd5b976b101c962b229b
Angelika Tyborska <atyborska93@gmail.com>
2015-12-13 13:31:39 +0100

    Add contribution validation to rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,8 +6,15 @@ class Rating < ActiveRecord::Base
   validates :project, presence: true
   validates :value, numericality: { only_integer: true, minimum: 1, maximum: 10 }
   validate :project_must_be_succeeded
+  validate :user_must_have_contributed_to_project
 
   def project_must_be_succeeded
     errors.add(:project, 'can\'t rate projects that did not succeed') unless project.succeed?
+  end
+
+  def user_must_have_contributed_to_project
+    unless user.paid_projects.include?(project)
+      errors.add(:project, 'can\t rate projects that you did not contribute to')
+    end
   end
 end
