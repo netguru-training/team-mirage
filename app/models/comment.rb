7b3fb7770104bdcd3883f3450b140510759795d7
barcadius <arkadiusz.gil95@gmail.com>
2015-12-13 14:30:22 +0100

    Rename sorted scope to sorted_by_date

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,5 +6,5 @@ class Comment < ActiveRecord::Base
   validates :project, presence: true
   validates :body, presence: true, length: { minimum: 2 }
 
-  scope :sorted, -> { order(created_at: :desc) }
+  scope :sorted_by_date, -> { order(created_at: :desc) }
 end
